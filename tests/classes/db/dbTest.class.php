fec3684ce176ab5489726fca75db6e27aa547641
Olivier <owenzek@gmail.com>
2015-03-25 19:49:37 +0100

    feature/02 - Updated db tests

--- tests/classes/db/dbTest.class.php
+++ tests/classes/db/dbTest.class.php
@@ -15,13 +15,26 @@
 
         }
 
-        public function testSetCampus() {
+        public function testSetDb() {
 
-            $db_slave = dbmanager::get_master();
-            $db_slave = dbmanager::get_slave();
+            $this->db_master = dbmanager::get_master();
+            $this->db_slave = dbmanager::get_slave();
 
             $this->assertInstanceOf('db', $this->db_master);
             $this->assertInstanceOf('db', $this->db_slave);
+
+        }
+
+        public function testGet() {
+
+            $this->db_master = dbmanager::get_master();
+            $this->db_slave = dbmanager::get_slave();
+
+            $game_master = $this->db_master->get('game', '', array('game_id = 1'), array(), array());
+            $game_slave = $this->db_slave->get('game', '', array('game_id = 1'), array(), array());
+
+            $this->assertEquals('test', $game_master['game_title']);
+            $this->assertEquals('test', $game_slave['game_title']);
 
         }
 
